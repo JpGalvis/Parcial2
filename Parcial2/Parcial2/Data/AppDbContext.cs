358cb44bbed736a2798ef404fe606e6fc9df5b42
jgalvissaavedra <jgalvissaavedra@gmail.com>
2021-10-13 21:15:29 -0400

    todo funcona bien hasta startup

---
+++ Parcial2/Parcial2/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Suerte> Suertes { get; set; } //Llama al modelo (Person) 
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+
+        {
+
+        }
+    }
+}
---
+++ Parcial2/Parcial2/Models/Suerte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public string futuro { get; set; }
+        [Display(Name = "Nombre completo del autor")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "El nombre debe contener entre 3 a 60 caracteres")]
+        [Required(ErrorMessage = "El autor es requerido")]
+        public string vision { get; set; }
+        [Url]
+        [Display(Name = "link del video")]
+        public string link { get; set; }
+    }
+}
